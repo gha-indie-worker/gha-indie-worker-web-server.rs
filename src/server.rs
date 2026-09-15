94f15ad1365401b11929ec0b989e89f4489ea0c4
Alexander Mills <alex@oresoftware.com>
2026-09-15 11:37:19 -0500

    feat: serve web and readiness routes

--- src/server.rs
+++ src/server.rs
@@ -2,9 +2,24 @@
 
 use crate::config::WebConfig;
 use crate::pages;
+use axum::{
+    http::StatusCode,
+    response::Html,
+    routing::get,
+    Router,
+};
+use std::error::Error;
 
-pub fn run(config: &WebConfig) {
-    println!("web bind {}", config.bind);
-    println!("{}", pages::home::markup());
+pub fn router() -> Router {
+    Router::new()
+        .route("/", get(|| async { Html(pages::home::markup()) }))
+        .route("/healthz", get(|| async { StatusCode::NO_CONTENT }))
+        .route("/readyz", get(|| async { StatusCode::NO_CONTENT }))
 }
 
+pub async fn run(config: &WebConfig) -> Result<(), Box<dyn Error>> {
+    let listener = tokio::net::TcpListener::bind(&config.bind).await?;
+    eprintln!("gha-indie-worker-web-server listening on {}", listener.local_addr()?);
+    axum::serve(listener, router()).await?;
+    Ok(())
+}
